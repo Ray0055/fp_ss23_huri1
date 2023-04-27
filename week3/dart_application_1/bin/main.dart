183b12d6ebf2bc40f70e04628fd3419bb7d05da0
Ray0055 <ruih_arbeit@126.com>
2023-04-28 00:48:39 +0200

    correct main.dart, remove test.py

--- week3/dart_application_1/bin/main.dart
+++ week3/dart_application_1/bin/main.dart
@@ -3,12 +3,12 @@ import 'dart:io';
 
 void main() {
   Dice dice = Dice(1, 6);
-  stdout.write('Enter numberOfThrows: ');
+  stdout.write('Enter numbedarOfThrows: ');
   int num = int.parse(stdin.readLineSync()!);
   dice.throwDice(false, num);
   print("equalDistr = ${dice.eqaulDistr}, numberOfThrows = ${dice.numberOfThrows},sumStatistics = ${dice.sumStatistics}");
   dice.resetStatistics();
-  dice.throwDice(true, 50000);
+  dice.throwDice(true, num);
   print("equalDistr = ${dice.eqaulDistr}, numberOfThrows = ${dice.numberOfThrows},sumStatistics = ${dice.sumStatistics}");
 
 }
